d8d0e5699a184d6e90b33e029f17a0cd081c2bf4
theghost2-mo <mohamedand20077@gmail.com>
2025-05-21 23:45:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// سكربت اختبار بسيط
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("تم تحميل وتشغيل السكربت.js");
+    const el = document.getElementById("js-status");
+    if (el) el.textContent = "تم تحميل وتشغيل السكربت بنجاح.";
+});
